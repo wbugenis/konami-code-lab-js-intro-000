fd5f49e402e73eed1e4f7ff6e4637288b18c660e
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-10-20 02:22:27 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,28 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+let index = 0;
+
+function konamiChecker(e){
+  const key = e.key;
+  
+  console.log(key);
+  if(key === codes[index]){
+    index++;
+    if(index === codes.length) {
+      alert("You're the best around. Nothing's ever gonna bring you down.")
+    }
+    
+    
+  } else {
+      index = 0;
+  }
 }
+
+    function init() {
+      document.body.addEventListener("keydown", (e) => {
+        konamiChecker(e);
+        }
+    );
+}
+
